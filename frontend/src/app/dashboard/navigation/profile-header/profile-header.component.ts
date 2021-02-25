0b02fbfcf41e79ff7c9b7b77547be7329d8affa9
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2021-02-25 15:24:08 +0300

    Dashboard sidenav commit
    Added a sidenav with navtabs

--- frontend/src/app/dashboard/navigation/profile-header/profile-header.component.ts
+++ frontend/src/app/dashboard/navigation/profile-header/profile-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-profile-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileHeaderComponent implements OnInit {
 
+  @Output() SideNavigationToggle = new EventEmitter();
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+  }
+
+  onToggleOpenSidenav() {
+
+    this.SideNavigationToggle.emit();
+
   }
 
 }
